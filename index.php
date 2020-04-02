118fa196d591e2c355c7b57eede462409d2a5475
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2020-04-02 08:52:13 -0800

    init commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+function getPost()
+{
+    if(!empty($_POST))
+    {
+        // when using application/x-www-form-urlencoded or multipart/form-data as the HTTP Content-Type in the request
+        // NOTE: if this is the case and $_POST is empty, check the variables_order in php.ini! - it must contain the letter P
+        return $_POST;
+    }
+
+    // when using application/json as the HTTP Content-Type in the request
+    $post = json_decode(file_get_contents('php://input'), true);
+    if(json_last_error() == JSON_ERROR_NONE)
+    {
+        return $post;
+    }
+
+    return [];
+}
+
+print_r(getPost());
+
+
+
+//$file = fopen("output.txt", "w");
+//
+//foreach ($_POST['feature']['attributes'] as $key => $var) {
+//    fwrite($file, "{$key}=>{$var}");
+//}
+//
+//fclose($file);
